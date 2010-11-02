18ddbde6af6ff414d7d752f7f9f9377e377f50cb
Manuel Kiessling <kiessling@myhammer.de>
2010-11-02 18:20:00 +0100

    tags and metatags working so far in the model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -8,14 +8,28 @@ class Tag < CouchRest::Model::Base
   view_by :name
 
   def self.sources_for_tagname(tagname)
-    tags = Tag.by_name(:name => tagname)
+    tags = Tag.by_name(:key => tagname)
     tag = Tag.find(tags[0]["_id"])
+    if (!tag.tags.empty?) then
+      event_sources = []
+      tag.tags.each do |subtag|
+        event_sources = event_sources | self.sources_for_tagname(subtag)
+      end
+      return event_sources
+    end
     tag.event_sources
   end
 
   def self.names_for_tagname(tagname)
-    tags = Tag.by_name(:name => tagname)
+    tags = Tag.by_name(:key => tagname)
     tag = Tag.find(tags[0]["_id"])
+    if (!tag.tags.empty?) then
+      event_names = []
+      tag.tags.each do |subtag|
+        event_names = event_names | self.names_for_tagname(subtag)
+      end
+      return event_names
+    end
     tag.event_names
   end
 
--- test/unit/tag_test.rb
+++ test/unit/tag_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class TagTest < ActiveSupport::TestCase
 
   setup do
+    teardown
     t = Tag.new
     t.name = "TESTcrons"
     t.event_sources << "TESTcron01"
@@ -10,10 +11,51 @@ class TagTest < ActiveSupport::TestCase
     t.event_names << "cpu_load"
     t.event_names << "free_mem"
     t.save
+
+    t = Tag.new
+    t.name = "TESTwebs"
+    t.event_sources << "TESTweb01"
+    t.event_sources << "TESTweb04"
+    t.event_names << "cpu_load"
+    t.event_names << "free_space"
+    t.save
+
+    t = Tag.new
+    t.name = "TESTother"
+    t.event_sources << "TESTother01"
+    t.event_sources << "TESTother04"
+    t.event_names << "cpu_load"
+    t.event_names << "free_space"
+    t.event_names << "mojo"
+    t.save
+
+    t = Tag.new
+    t.name = "TESTmeta"
+    t.tags << "TESTcrons"
+    t.tags << "TESTwebs"
+    t.save
   end
 
   teardown do
-    tags = Tag.by_name(:name => "TESTcrons")
+    tags = Tag.by_name(:key => "TESTcrons")
+    tags.each do |tag|
+      t = Tag.find(tag["_id"])
+      t.destroy
+    end
+
+    tags = Tag.by_name(:key => "TESTwebs")
+    tags.each do |tag|
+      t = Tag.find(tag["_id"])
+      t.destroy
+    end
+
+    tags = Tag.by_name(:key => "TESTother")
+    tags.each do |tag|
+      t = Tag.find(tag["_id"])
+      t.destroy
+    end
+
+    tags = Tag.by_name(:key => "TESTmeta")
     tags.each do |tag|
       t = Tag.find(tag["_id"])
       t.destroy
@@ -30,6 +72,20 @@ class TagTest < ActiveSupport::TestCase
   test "must return array with names" do
     actual = Tag.names_for_tagname("TESTcrons")
     expected = ["cpu_load", "free_mem"]
+
+    assert_equal(expected, actual)
+  end
+
+  test "must return array with sources for meta tag" do
+    actual = Tag.sources_for_tagname("TESTmeta")
+    expected = ["TESTcron01", "TESTcron02", "TESTweb01", "TESTweb04"]
+
+    assert_equal(expected, actual)
+  end
+
+  test "must return array with names for meta tag" do
+    actual = Tag.names_for_tagname("TESTmeta")
+    expected = ["cpu_load", "free_mem", "free_space"]
 
     assert_equal(expected, actual)
   end
