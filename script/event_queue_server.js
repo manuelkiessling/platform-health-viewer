e540a626c66489dc8664a91391103ae89c039cc2
Manuel Kiessling <kiessling@myhammer.de>
2010-11-22 16:39:14 +0100

    node.js event queue server basically working

--- script/event_queue_server.js
+++ script/event_queue_server.js
@@ -12,18 +12,29 @@ db.open("../db/development.sqlite3", function (error) {
   }
 
   http.createServer(function(request, response) {
-    console.log("Received request...");
-    db.execute
-    ( "INSERT INTO queue_events (source, name, value, created_at) VALUES (?, ?, ?, ?)",
-      ['cron01', 'cpu_load', '1.99', '2010-11-01 10:06:01.465355'],
-      function (error, rows) {
-        if (error) throw error;
-        console.log("Event added to queue.");
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("Event added to queue.");
-        response.end();
+    console.log("Received request: " + request.url);
+    var q = url.parse(request.url, true);
+    if (q.query) {
+      if (q.query.source && q.query.name && q.query.value) {
+        console.log("Source: " + q.query.source);
+        console.log("Name: " + q.query.name);
+        console.log("Value: " + q.query.value);
+
+        db.execute
+        ( "INSERT INTO queue_events (source, name, value, created_at) VALUES (?, ?, ?, strftime('%s','now'))",
+          [q.query.source, q.query.name, q.query.value],
+          function (error, rows) {
+            if (error) throw error;
+            console.log("Event added to queue.");
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write("Event added to queue.");
+            response.end();
+          }
+        );
       }
-    );
+    } else {
+      console.log("Ignoring request: " + request.url);
+    }
   }).listen(8888);
 });
 
