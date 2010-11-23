5164c38682306d8356a708868abe4cb4ce6c7654
Manuel Kiessling <kiessling@myhammer.de>
2010-11-23 17:06:28 +0100

    Event presentation on tags page is now truly separated from tags view, only json is exchanged

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,15 +24,23 @@ class EventsController < ApplicationController
   def latest
     @tag_id = params[:tag_id]
     tag_name = Tag.find(@tag_id).name
-    @events = Event.all(:conditions => {
-                          :event_type_id => EventType.find_by_sources_and_names(
-                                              Tag.sources_for_tagname(tag_name),
-                                              Tag.names_for_tagname(tag_name)
-                                            )
-                        },
-                        :order => "id DESC",
-                        :limit => 10
-                      )
+    found_events = Event.all(:conditions => {
+                             :event_type_id => EventType.find_by_sources_and_names(
+                                                 Tag.sources_for_tagname(tag_name),
+                                                 Tag.names_for_tagname(tag_name)
+                                               )
+                             },
+                             :order => "id DESC",
+                             :limit => 10
+                            )
+    @events = []
+    found_events.each do |event|
+      enriched_event = event.serializable_hash
+      enriched_event["name"] = event.event_type.name
+      enriched_event["source"] = event.event_type.source
+      @events << enriched_event
+    end
+
     respond_to do |format|
       format.html
       format.js
