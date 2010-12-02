0215d4637823ae36188d9fe32f18d1933b831263
Manuel Kiessling <kiessling@myhammer.de>
2010-12-02 23:10:08 +0100

    more finetuning

--- app/helpers/frames_helper.rb
+++ app/helpers/frames_helper.rb
@@ -1,2 +1,23 @@
 module FramesHelper
+
+  def prepare_events_for_raphael(event_groups, num_cols)
+    x = []
+    data = []
+    x << num_cols.times.collect
+    zerodata = []
+    num_cols.times { zerodata << 0 }
+    data << zerodata
+    event_groups.each_pair do |event_type_id, values|
+      gx = []
+      gdata = []
+      values.keys.sort.each do |key|
+        gx << key
+        gdata << values[key]
+      end
+      x << gx
+      data << gdata
+    end
+    return {"x" => x.inspect.to_s, "data" => data.inspect.to_s}
+  end
+
 end
