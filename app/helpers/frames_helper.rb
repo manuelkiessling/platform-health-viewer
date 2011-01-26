6d7b0dcfa865dc362bb1479ab340ee40f4d604b5
Manuel Kiessling <manuel@kiessling.net>
2011-01-26 18:15:59 +0100

    Graph colors now working, better layout

--- app/helpers/frames_helper.rb
+++ app/helpers/frames_helper.rb
@@ -23,16 +23,25 @@ module FramesHelper
   def get_event_type_names_for_google_graph(averaged_events)
     event_type_names = ""
     averaged_events.each_pair do |i, averaged_event|
-      event_type_names = event_type_names + averaged_event["event_type_id"].to_s + "|"
+      event_type = EventType.find(averaged_event["event_type_id"])
+      event_type_names = event_type_names + event_type.source + ": " + event_type.name + "|"
     end
     return event_type_names[0..-2]
+  end
+
+  def get_event_type_colors_for_google_graph(averaged_events)
+    event_type_colors = ""
+    averaged_events.each_pair do |i, averaged_event|
+      event_type = EventType.find(averaged_event["event_type_id"])
+      event_type_colors = event_type_colors + ("%06x" % (rand * 0xffffff)).to_s + ","
+    end
+    return event_type_colors[0..-2]
   end
   
   def get_event_type_average_values_for_google_graph(averaged_events)
     event_type_average_values = ""
 
     averaged_events.each_pair do |i, averaged_event|
-      event_type_average_values = event_type_average_values + "-1|"
       averaged_event["values"].each_pair do |j, value|
         event_type_average_values = event_type_average_values.to_s + value["value"].to_s + ","
       end
